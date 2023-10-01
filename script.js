9eb53a86aa05a65e9d1b622052fb47d02bd33228
Dniemy <146644576+Dniemy@users.noreply.github.com>
2023-10-01 16:09:13 +0200

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+/* NAVBAR */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n. 
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active")
+    }))
+
+
 /* SLIDER */
 
 const wrapper = dodument.querySelector('.wrapper')
@@ -30,22 +48,6 @@ wrapper.addEventListener('mousemove', function (e) {
 
 
 
-/* NAVBAR */
-
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n. 
-    addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active")
-    }))
 
 
 /* CONTACT FORM */
